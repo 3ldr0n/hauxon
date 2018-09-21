8d14061bdcbcd92c6f483acb79063657284d18a2
Edison Neto <ednetoali@gmail.com>
2018-09-21 02:53:47 -0300

    Change while for 'for' loops

--- src/yes.c
+++ src/yes.c
@@ -4,10 +4,10 @@ int main(int argc, char **argv)
 {
 
 	if (argc == 1) {
-		while (1)
+		for (;;)
 			printf("y\n");
 	} else {
-		while (1) {
+		for (;;) {
 			for (int i=1;i<argc;i++)
 				printf("%s ", argv[i]);
 			printf("\n");
