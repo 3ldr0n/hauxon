53572c733604cc0ed2ff3076c52a2c5629277cad
Edison Neto <ednetoali@gmail.com>
2018-12-02 10:55:40 -0200

    Add cat header file back
    Decided to put the header again, but this time it is in the src
    directory.

--- src/cat.c
+++ src/cat.c
@@ -1,17 +1,4 @@
-#define _GNU_SOURCE
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <unistd.h>
-
-#define PROGRAM "cat"
-#define MAX_BUFFER_SIZE 32
-
-static FILE *fopen_s(char *filename, char *open_mode);
-static void cat(FILE *in, uint8_t n);
-void usage(void);
+#include "cat.h"
 
 int
 main(int argc, char **argv)
