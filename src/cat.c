638f2fdbe74345710429f8c60986df448b378b27
Edison Neto <ednetoali@gmail.com>
2018-12-02 10:53:52 -0200

    Remove cat.h include and add headers on .c file

--- src/cat.c
+++ src/cat.c
@@ -1,4 +1,17 @@
-#include "cat.h"
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PROGRAM "cat"
+#define MAX_BUFFER_SIZE 32
+
+static FILE *fopen_s(char *filename, char *open_mode);
+static void cat(FILE *in, uint8_t n);
+void usage(void);
 
 int
 main(int argc, char **argv)
