8cc987c49a914ec7675c79427be3016e84eeaefe
Edison Neto <ednetoali@gmail.com>
2019-01-10 11:57:15 -0200

    Add -h option for the free program

--- src/free.c
+++ src/free.c
@@ -10,12 +10,13 @@
 
 void usage(void);
 void xfree(int unit);
+void pretty_free();
 
 int main(int argc, char **argv)
 {
     int opt;
     int unit = 1;
-    while ((opt = getopt(argc, argv, "kmg")) != -1) {
+    while ((opt = getopt(argc, argv, "kmgh")) != -1) {
         switch (opt) {
         case 'k':
             break;
@@ -25,6 +26,9 @@ int main(int argc, char **argv)
         case 'g':
             unit = 1024*1024;
             break;
+		case 'h':
+			pretty_free();
+			return 0;
         default:
             usage();
             exit(EXIT_FAILURE);
@@ -67,6 +71,34 @@ void xfree(int unit)
            totalswap/unit, freeswap/unit, usedswap/unit);
 }
 
+void pretty_free()
+{
+	int unit = 1024*1024;
+	struct sysinfo info;
+	int error = sysinfo(&info);
+	if (error) {
+		fprintf(stderr, "Code error: %d\n", error);
+		exit(EXIT_FAILURE);
+	}
+
+    float totalram = (float)(info.totalram * info.mem_unit) / 1024;
+    float freeram = (float)(info.freeram * info.mem_unit) / 1024;
+    float bufferram = (float)(info.bufferram * info.mem_unit) / 1024;
+
+    float totalswap = (float)(info.totalswap * info.mem_unit) / 1024;
+    float freeswap = (float)(info.freeswap * info.mem_unit) / 1024;
+
+    float usedram = totalram - freeram - bufferram;
+    float usedswap = totalswap - freeswap;
+
+    printf("\ttotal\tfree\tused\n");
+    printf("Mem:\t%.2fG\t%.2fG\t%.2fG\n",
+           totalram/unit, freeram/unit, usedram/unit);
+
+    printf("Swap:\t%.2fG\t%.2fG\t%.2fG\n",
+           totalswap/unit, freeswap/unit, usedswap/unit);
+}
+
 void usage(void)
 {
     printf("\nUsage: free [OPTIONS]\n");
@@ -75,4 +107,5 @@ void usage(void)
     printf(" -k\tdisplay numbers in kibibytes.\n");
     printf(" -m\tdisplay numbers in megibytes.\n");
     printf(" -g\tdisplay numbers in gibibytes.\n");
+    printf(" -h\tdisplay numbers in a pretty way.\n");
 }
