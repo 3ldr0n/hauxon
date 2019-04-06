727184496ab9a3504e65ff667e0c9e5d03dfbf91
Edison Neto <ednetoali@gmail.com>
2019-04-06 15:07:20 -0300

    Fix issues with tail

--- src/tail.c
+++ src/tail.c
@@ -1,39 +1,36 @@
 #define _GNU_SOURCE
 
 #include <stdio.h>
-#include <stdint.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #define PROGRAM "head"
 
-static void tail(FILE *in, uint8_t n);
+static void tail(FILE *in);
 static FILE *fopen_s(char *filename, char *open_mode);
 
 int main(int argc, char **argv)
 {
     int opt;
-    uint8_t n = 0;
 
     while ((opt = getopt(argc, argv, "n")) != -1) {
         switch (opt) {
-        case 'n':
-            n = 1;
-            break;
         default:
             exit(EXIT_FAILURE);
         }
     }
 
     if (optind >= argc) {
-        tail(stdin, n);
+        tail(stdin);
         return 0;
     }
 
     char **file = &argv[optind];
     for ( ; *file ; file++) {
         FILE *fp = fopen_s(*file, "r");
-        tail(fp, n);
+        tail(fp);
         fclose(fp);
     }
 
@@ -54,23 +51,23 @@ static FILE
 }
 
 static void
-tail(FILE *in, uint8_t n)
+tail(FILE *in)
 {
     ssize_t read;
     char *buff = NULL;
     size_t size = 0;
-    char **file_content = malloc(10*sizeof(char *));
-
-    for (int i = 0;i < 10;i++)
-        *(file_content+i) = malloc(sizeof(char *));
+    char **file_content = NULL;
 
     int i  = 0;
     while ((read = getline(&buff, &size, in)) != -1) {
-        if (i == 10)
-            break;
+        file_content = (char **)realloc(file_content, (i + 1) * sizeof(char *));
+        file_content[i] = (char *)calloc(sizeof(char), strlen(buff));
 
-        file_content[i] = buff;
-        printf("%s", *(file_content+i));
+        strcpy(file_content[i], buff);
         i++;
+    }
+
+    for (int j = (i-10);j < i;j++) {
+        printf("%s", file_content[j]);
     }
 }
